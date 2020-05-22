b636b2a159a07dcf79dc821a5fa59e38208eba6b
dvdocumentation <65721109+dvdocumentation@users.noreply.github.com>
2020-05-22 16:17:12 +0300

    Add files via upload

--- conf.py
+++ conf.py
@@ -57,6 +57,9 @@ master_doc = 'index'
 #
 html_theme = 'alabaster'
 
+
+
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
